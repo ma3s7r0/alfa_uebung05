33f2f8fd4098a09039507bf1b19db28712511a39
Nico <alfatraining@fancydomain.de>
2020-12-22 16:05:21 +0100

    Only Data Exchange (not working!)

---
+++ src/components/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Table from './Table';
+
+function Cart({cart, clickFunction, clickText}) {
+    return (
+        <>
+
+            {cart.length !== 0 && <Table data={cart} clickFunction={clickFunction} clickText={clickText}/>}
+            {cart.length === 0 && <p>Der Warenkorb ist leer.</p>}
+        
+{/*             <h2>Warenkorb</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titel</th>
+                        <th>Preis</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table> */}
+
+            
+        </>
+    );
+}
+
+export default Cart;
